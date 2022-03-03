291eaaf79c2d65874e2ec036bb21175eff279f44
Matic Morgan <100137377+MaticMorganIJS@users.noreply.github.com>
2022-03-03 11:50:52 +0100

    some updates, still w.i.p.
    Second update:
    the characteristic times of exponential growth/fall are now calculated and printed in seconds. The average of all characteristic times for a set of files is also calculated and printed out.
    Files can now have a header with every line starting with the '#' symbol and the program can still extract the correct data.

--- data_processing_relaxation.py
+++ data_processing_relaxation.py
@@ -11,16 +11,19 @@ from scipy.optimize import curve_fit
 import numpy as np
 
 
-# exponential function
+ # exponential function
 def exponentialFunction(x, m, t, b):
     return m * np.exp(-t * x) + b
 
 
-# function that extracts consecutive forces which are measured every 0.1 seconds
+ # function that extracts consecutive forces which are measured every 0.1 seconds
 def ExtractForces(file, force_type):
     forces = []
     for line in vsebina:
-        forces.append(float(line.split('\t')[0]))
+        if line[0] == '#':
+            continue
+        else:
+            forces.append(float(line.split('\t')[0]))
     
     if (force_type == 'all'):
         return forces
@@ -28,13 +31,13 @@ def ExtractForces(file, force_type):
     elif (force_type == 'upper'):
         upper_forces = []
         for idx, val in enumerate(forces):
-            if (idx < (len(forces) / 2) + 10):
+            if (idx < (len(forces) / 2)):
                 upper_forces.append(val)
         
-        # cutting away the initial rising forces
+         # cutting away the initial rising forces
         while (upper_forces[0] < upper_forces[1]):
             upper_forces.pop(0)
-        # cutting away the latter falling forces
+         # cutting away the latter falling forces
         while (upper_forces[-1] < upper_forces[-2]):
             upper_forces.pop(-1)
             
@@ -45,14 +48,14 @@ def ExtractForces(file, force_type):
         for idx, val in enumerate(forces):
             if (idx > (len(forces) / 2)):
                 lower_forces.append(val)
-        # cutting away the initial falling forces
+         # cutting away the initial falling forces
         while (lower_forces[0] > lower_forces[1]):
             lower_forces.pop(0)
         
         return lower_forces
 
 
-# function that draw the specified graph
+ # function that draw the specified graph
 def DrawForces(force_type):
     
     if (force_type == 'all'):
@@ -61,45 +64,56 @@ def DrawForces(force_type):
         ax.set_title('Force with respect to time')
         plt.xlabel('Time [ 0.1 s ]')
         plt.ylabel('Force [ mg ]')
-        plt.scatter(range(len(ExtractForces(file, 'all'))), ExtractForces(file, 'all'), s=1)
+        xs = np.linspace( 0, len(ExtractForces(file, 'all')) / 10, num=len(ExtractForces(file, 'all')) )
+        plt.scatter(xs, ExtractForces(file, 'all'), s=1)
+    
     else:
-        
         fig, ax = plt.subplots()
         ax.set_title('Forces during relaxation with respect to time')
-        plt.xlabel('Time [ 0.1 s ]')
+        plt.xlabel('Time [ s ]')
         plt.ylabel('Force [ mg ]')
         
         ys = ExtractForces(file, force_type)
-        xs = range(len(ys))
-        # perform the fit
-        p0 = (2000, .1, 50) # start with values near those we expect
+        xs = np.linspace( 0, len(ExtractForces(file, force_type)) / 10, num=len(ExtractForces(file, force_type)) )
+        p0 = (2000, .1, 50)
         params, cv = curve_fit(exponentialFunction, xs, ys, p0)
         m, t, b = params
                 
-        # plot the results
+         # plot the results
         plt.scatter(xs, ys, s=1, label="data")
         plt.plot(xs, exponentialFunction(xs, m, t, b), '--', label="fitted", color = 'r')
         plt.legend()
         
-        # calculate and print out the parameters
-        sampleRate = 20_000 # Hz
-        tauSec = (1 / t) / sampleRate
-        print(f"Y = {m} * e^(-{t} * x) + {b}")
-        print(f"Tau = {tauSec * 1e6} µs")
-
+         # calculate and print out the parameters
+        tau = 1 / t
+        print(f" Tau_{force_type} = {tau} s")
+        
+        return tau
        
+
 
 if __name__ == "__main__":
     
+    all_upper_taus = []
+    all_lower_taus = []
     
-    # processing measurements from all files in the specified directory
+     # processing measurements from all files in the specified directory
     for entry in os.scandir(r'C:\Users\Matic\Documents\aging\meritve_aging\measurements_to_be_processed'):
         file = open(entry.path)
         vsebina = file.readlines()
         
-        DrawForces('all')
-        DrawForces('upper')
-        DrawForces('lower')
-      
+        try:
+            DrawForces('all')
+            all_upper_taus.append(DrawForces('upper'))
+            all_lower_taus.append(DrawForces('lower'))
+        except:
+            print(f"File {file} couldn't be processed.")
+            file.close()
+        finally:        
+            file.close()
+            
+        average_upper_tau = sum(all_upper_taus) / len(all_upper_taus)
+        average_lower_tau = sum(all_lower_taus) / len(all_lower_taus)
         
-        file.close()        
+    print(average_upper_tau)
+    print(average_lower_tau)
