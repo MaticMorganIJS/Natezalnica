d27c32cf847b16df4fd8cf992bf42dc17e5060dc
Matic Morgan <100137377+MaticMorganIJS@users.noreply.github.com>
2022-03-04 19:03:26 +0100

    Version 2.1
    Added functions that can read if a file has been created during a measurement when the sample broke and can extract the good measurements from this same file. Also removed some old, outdated functions.

--- data_processing_aging.py
+++ data_processing_aging.py
@@ -45,7 +45,7 @@ def ExtractTimes(file, time_type):
     else:
         return lower_times
     
-def ExtractLengths(file, time_type):
+def ExtractLengths(file, length_type):
     upper_lengths = []
     lower_lengths = []
     for idx, line in enumerate(vsebina):
@@ -57,14 +57,38 @@ def ExtractLengths(file, time_type):
             else:
                 lower_lengths.append(float(line.split('\t')[2]))
     
-    if (time_type == 'upper'):
+    if (length_type == 'upper'):
         return upper_lengths
     else:
         return lower_lengths
 
-# functions that calculate certain values
 
-# strain value coresponding to the %, area in mm^2, returns value in GPa
+def IsBroken(file):
+    upper_forces = ExtractForces(file, 'upper')
+    lower_forces = ExtractForces(file, 'lower')
+    lower_forces_avg = sum(lower_forces) / len(lower_forces)
+    broken = False
+    for u in upper_forces:
+        if (abs(u - lower_forces_avg) < 1000):
+            broken = True
+            
+    return broken
+
+def ExtractGoodForces(file):
+    if IsBroken(file):
+        upper_forces = ExtractForces(file, 'upper')
+        lower_forces = ExtractForces(file, 'lower')
+        lower_forces_avg = sum(lower_forces) / len(lower_forces)
+        while (abs(upper_forces[-1] - lower_forces_avg) < 1000):
+            upper_forces.pop(-1)
+        
+        return upper_forces
+            
+            
+            
+ # functions that calculate certain values
+
+ # strain value coresponding to the %, area in mm^2, returns value in GPa
 def CalculateYoungModulus(file, strain, area):
     moduli = []
     forces = ExtractForces(file, 'upper')
@@ -73,7 +97,7 @@ def CalculateYoungModulus(file, strain, area):
     
     return moduli
 
-# functions that draw the specified graphs
+ # functions that draw the specified graphs
 def draw_forces(force_type):
     if (force_type == 'upper'):
         fig, ax = plt.subplots()
@@ -106,22 +130,6 @@ def draw_lengths(length_type):
         plt.scatter(range(len(all_lower_lengths)), all_lower_lengths, s=1)
 
 
-def draw_additional_relaxation_time(): 
-    fig, ax = plt.subplots()
-    ax.set_title('Additional relaxation time')
-    plt.xlabel('Consecutive measurement [ / ]')
-    plt.ylabel('Additional relaxation time [ s ]')
-    plt.scatter(range(len(all_additional_relaxations)), all_additional_relaxations, s=1)
-    
-    
-def draw_oscillation_time():
-    fig, ax = plt.subplots()
-    ax.set_title('Oscillation time')
-    plt.xlabel('Consecutive measurement [ / ]')
-    plt.ylabel('Oscillation time [ s ]')
-    plt.scatter(range(len(all_oscillation_times)), all_oscillation_times, s=1)
-
-
 def draw_young_modulus():
     fig, ax = plt.subplots()
     ax.set_title('Young\'s modulus with respect to the number of repetitions')
@@ -131,33 +139,31 @@ def draw_young_modulus():
 
 if __name__ == "__main__":
     
-    all_additional_relaxations = []
     all_upper_forces = []
     all_lower_forces = []
-    all_oscillation_times = []
     all_upper_lengths = []
     all_lower_lengths = []
     all_moduli = []
     
-    # adding up measurements from all files in the specified directory
-    for entry in os.scandir(r'C:\Users\Matic\Documents\aging\meritve_aging\measurements_to_be_processed'):
+     # adding up measurements from all files in the specified directory
+    for entry in os.scandir(r'C:\Users\Matic\Documents\aging\meritve_aging\measurements_to_be_processed\aging'):
         file = open(entry.path)
         vsebina = file.readlines()
-
-        all_additional_relaxations += extract_additional_relaxations(file)
-        all_upper_forces += extract_forces(file, 'upper')
-        all_lower_forces += extract_forces(file, 'lower')
-        all_oscillation_times += extract_times(file, 'oscillation')
-        all_upper_lengths += extract_lengths(file, 'upper')
-        all_lower_lengths += extract_lengths(file, 'lower')
-        all_moduli += calculate_youngs_moduli(file, 30, 1.6)
-        file.close()
-        
-    # drawing all the graphs
+            
+        try:
+            all_upper_forces += ExtractForces(file, 'upper')
+            all_lower_forces += ExtractForces(file, 'lower')
+            all_moduli += CalculateYoungModulus(file, 30, 1.6)
+            file.close()
+        except:
+            print(f"File {file} couldn't be processed.")
+            file.close()
+        finally:
+            file.close()
+            
+            
+     # drawing all the graphs
     draw_forces('upper')
     draw_forces('lower')
-    draw_lengths('upper')
-    draw_lengths('lower')
-    draw_additional_relaxation_time()
-    draw_oscillation_time()
+    
     draw_young_modulus()
